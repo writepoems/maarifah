640e726bb7df185dd1df2e04b0342439c864b516
writepoems <272058035+writepoems@users.noreply.github.com>
2026-04-23 08:59:11 -0500

    add surah endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
-// This file is still a work-in-progress
-// For now, there is a "Hello, world" test
-
 import { Hono } from "hono"
+import { db } from "./database"
 
 const app = new Hono()
-app.get("/", c => c.text("Hello, world"))
+
+app.get("/", async c => {
+  const res = await db.query("SELECT * FROM quran")
+
+  return c.json({ 
+    count: res.rowCount 
+  })
+})
+
+app.get("/surah/:no", async c => {
+  const surahNo = c.req.param("no")
+  const res = await db.query("SELECT * FROM quran WHERE surah_no = $1", [surahNo])
+
+  return c.json(res.rows)
+})
 
 export default app

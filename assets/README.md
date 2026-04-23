81a740608086d9ea0452c038e03657d47371666e
writepoems <272058035+writepoems@users.noreply.github.com>
2026-04-23 12:40:02 -0500

    create table directly instead of having an sql file

--- assets/README.md
+++ assets/README.md
@@ -6,6 +6,6 @@ This directory contains the dataset, setup script, and other additional files us
 
 1. Configure `config.example.json` (and rename to `config.json`)
 2. Run `setup.ts` after installing `pg` and `udsv`
-3. Test whether or not everything was imported by running the script again
+3. Test whether or not everything was imported by running the script again, or check your database manually
 
 Alternatively, you can do `bun db:setup` to do this automatically.
--- assets/setup.ts
+++ assets/setup.ts
@@ -48,7 +48,24 @@ async function main() {
   await pg.connect()
 
   console.log("Connected to", pg.host)
-  pg.query(await Bun.file("./table.sql").text())
+  
+  pg.query(`
+    CREATE TABLE IF NOT EXISTS quran (
+      id                  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      surah_no            INT,
+      surah_name_en       TEXT,
+      surah_name_ar       TEXT,
+      surah_name_roman    TEXT,
+      ayah_no_surah       INT,
+      ayah_no_quran       INT,
+      ayah_ar             TEXT,
+      ayah_en             TEXT,
+      total_ayah_surah    INT,
+      total_ayah_quran    INT,
+      place_of_revelation TEXT
+    );
+  `)
+
   console.log("Created table \"quran\"")
 
   const q = await pg.query("SELECT * FROM quran")
